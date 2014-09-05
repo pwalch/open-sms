cf4b880e81d8fabe03d8da68c8a9a88ee69ac337
Pierre Walch <pwalch@gmail.com>
2014-09-05 15:39:32 +0200

    Add contact add activity class

---
+++ app/src/main/java/pwalch/net/opensms/ContactActivity.java
@@ -0,0 +1,58 @@
+package pwalch.net.opensms;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import pwalch.net.opensms.structures.Contact;
+
+
+public class ContactActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+
+        Button button = (Button) findViewById(R.id.send_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText contactName = (EditText) findViewById(R.id.contact_name);
+                EditText contactNumber = (EditText) findViewById(R.id.contact_number);
+
+                // TODO find an available conversation filename, create Application class
+                // to share data between activities
+                //Contact contact = new Contact(contactName.getText(), contactNumber.getText());
+                contactName.setText("");
+                contactNumber.setText("");
+
+
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.contact, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
